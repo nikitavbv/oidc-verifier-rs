8aec5e42c7d2757a35a811a09fe7eba890d3e4b1
Nikita Volobuev <nikitavbv@gmail.com>
2021-07-10 18:36:16 +0300

    feat(oidc-verifier-rs): actually verify tokens

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,30 @@
 // see https://developers.cloudflare.com/cloudflare-one/identity/users/validating-json
 
 use crate::certs::CertsResponse;
+use std::collections::HashSet;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 mod certs;
 
 pub struct OIDCTokenVerifier {
     certs_url: String,
+    auds: HashSet<String>,
     keys: Vec<jsonwebkey::JsonWebKey>,
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+pub struct TokenClaims {
+    aud: Vec<String>,
+    exp: u64,
+    email: String,
 }
 
 impl OIDCTokenVerifier {
 
-    pub fn new(certs_url: &str) -> Self {
+    pub fn new(certs_url: &str, auds: HashSet<String>) -> Self {
         OIDCTokenVerifier {
             certs_url: certs_url.to_string(),
+            auds,
             keys: Vec::new(),
         }
     }
@@ -37,28 +48,49 @@ impl OIDCTokenVerifier {
 
         None
     }
+
+    pub fn verify(&self, token: &str) -> Option<TokenClaims> {
+        let validation = jsonwebtoken::Validation::new(jsonwebtoken::Algorithm::RS256);
+        let header = jsonwebtoken::decode_header(&token).expect("failed to decode header");
+
+        let result = jsonwebtoken::decode::<TokenClaims>(
+            &token,
+            &self.key_by_id(&header.kid.unwrap()).unwrap().key.to_decoding_key(),
+            &validation
+        ).expect("failed to decode");
+
+        let contains_aud = result.claims.aud.iter()
+            .map(|v| self.auds.contains(v))
+            .reduce(|a, b| a || b)
+            .unwrap_or(false);
+
+        if !contains_aud {
+            return None;
+        }
+
+        let time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
+
+        if result.claims.exp < time {
+            return None;
+        }
+        
+        Some(result.claims)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::OIDCTokenVerifier;
 
-    #[derive(serde::Serialize, serde::Deserialize, Debug)]
-    struct TokenClaims {}
-
     #[tokio::test]
     async fn request_keys() {
-        let mut token_verifier = OIDCTokenVerifier::new("https://api.nikitavbv.com/cdn-cgi/access/certs");
+        let mut token_verifier = OIDCTokenVerifier::new(
+            "https://api.nikitavbv.com/cdn-cgi/access/certs",
+            ["dd40dd06f1cc22637a82c6978b379ef5ca838099d8b4bd81f6af3a0bb0642ecc".to_string()].iter().cloned().collect()
+        );
         token_verifier.request_keys().await;
 
         let test_token_str = std::env::var("TEST_TOKEN").expect("Expected TEST_TOKEN to be set");
-        let validation = jsonwebtoken::Validation::new(jsonwebtoken::Algorithm::RS256);
-
-        let header = jsonwebtoken::decode_header(&test_token_str).expect("failed to decode header");
-
-        let result = jsonwebtoken::decode::<TokenClaims>(&test_token_str, &token_verifier.key_by_id(&header.kid.unwrap()).unwrap().key.to_decoding_key(), &validation)
-            .expect("failed to decode");
-
-        println!("result: {:?}", result);
+        assert!(token_verifier.verify(&test_token_str).is_some());
     }
 }
