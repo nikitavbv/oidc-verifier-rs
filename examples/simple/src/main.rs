a040f32223b2f40d6f96173fc85ac14ac3c2c690
Nikita Volobuev <nikitavbv@gmail.com>
2021-08-04 08:26:41 +0300

    fix(example): fix compile errors

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -1,13 +1,18 @@
+use log::*;
+
 use oidc_verifier_rs::{OIDCTokenVerifier, TokenVerificationResult};
 
+#[tokio::main]
 async fn main() {
+    env_logger::init();
+
     let token_verifier = OIDCTokenVerifier::new(
         "https://api.nikitavbv.com/cdn-cgi/access/certs",
         [
             "key1".to_string(),
             "key2".to_string()
         ].iter().cloned().collect()
-    ).await;
+    ).await.unwrap();
 
     let token = match token_verifier.verify("some-token") {
         TokenVerificationResult::Ok(v) => v,
